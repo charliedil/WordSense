5a08cdf33bc537d62ae048c39e869bb2cdd856b3
charliedil <charliedil2019@gmail.com>
2022-04-02 16:11:40 -0400

    Organized contexts into dict based on sense. This allows me to also access the most frequent sense.

--- wsd.py
+++ wsd.py
@@ -11,5 +11,21 @@ train_file.close()
 
 ## Parse training file
 
+context_to_label = {"phone":[],"product":[]}
+
+last_label = ""
+context = False
 train_lines = train_file_text.split("\n")
 for line in train_lines:
+    if context:
+        context_to_label[last_label].append(line)
+        context = False
+        last_label = ""
+    if line.startswith("<answer"):
+        last_label = re.search(r"senseid=\"(.*)\"", line).group(1)
+    if line.startswith("<context>"):
+        context = True
+
+
+print(len(context_to_label["product"]))
+print(len(context_to_label["phone"]))
